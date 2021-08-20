4411a5997231ce975d2da062bd09319039098e10
sohyeonBak <sohyeonbakoly@gmail.com>
2021-08-20 21:42:54 +0900

    리덕스 데이터 전달 셋팅

--- shoppingMall-frontend/src/sagas/user.tsx
+++ shoppingMall-frontend/src/sagas/user.tsx
@@ -1,15 +1,15 @@
 import axios from "axios";
 import { all, call, fork, put, takeLatest } from "redux-saga/effects";
-import { SIGN_UP_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS } from "../reducers/user";
+import { signUpAsync, SIGN_UP_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS } from "../reducers/user";
 
 
 function signUpAPI(data:any) {
   return axios.post('/categories', data)
 }
 
-function* signup(action:any) {
+function* signup(action: any) {
   try{
-    // const result = yield call(signUpAPI, action.data);
+    // const result = yield call(signUpAPI, action.data)
     // console.log(result);
     yield put({
       type:SIGN_UP_SUCCESS,
